c952f02cae60bfe199fdbd33e7df5df86fd7d716
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2023-12-03 22:35:08 +0900

    feat: db연결 테스트

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const connection = require('./db.js');
+
 const port = 3001;
 
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
+    const users = await connection.query('SELECT * FROM users');
+    console.log(users);
     res.send('Hello, Express!');
 });
 

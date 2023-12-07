9d0fcac0b539703c6285f37ac980f9305e8fd331
JeanYoungPark <jean.young.park.96@gmail.com>
2023-12-07 14:37:06 +0900

    feat: 관리자 회원가입, 수정, 리스트 작업 완료

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import { app } from '#core/index.js';
-import { findUser } from '#route/user.js'
+import { findUser,updateUser } from '#route/user.js'
 import { adminJoinRouter } from '#route/admin/join.js';
 import { adminListRouter } from '#route/admin/list.js';
 
 app.use(findUser);
+app.use(updateUser);
 
 // admin
 app.use(adminJoinRouter);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { User } from '#model/User.js';
+import bcrypt from 'bcrypt';
 
 export const findUser = express.Router();
+export const updateUser = express.Router();
 
 findUser.post('/api/find/user', async (req, res) => {
     const data = req.body;
@@ -11,21 +13,19 @@ findUser.post('/api/find/user', async (req, res) => {
     });
 
     return res.status(200).json({ user });
-
 })
 
-userUpdate.post('/api/user/update', async (req, res) => {
+updateUser.post('/api/user/update', async (req, res) => {
     const data = req.body;
-    // 여기서 값 받아서 update해야함,.. view페이지 회원가입페이지 수정 페이지 분리 필요
-    const user = new User();
-    
-    try {
-        const result = await user.update();
-        return res.status(200).json({ result });
-    } catch (error) {
-        return res.status(200).json({ message: '사용자 저장 중 오류 발생', error: error.message });
+    const user = await User.findOne({ where: { id: data.id} });
+    const isMatch = bcrypt.compare(user.password, data.password);
+
+    if(isMatch){
+        try {
+            const result = await user.update(data, {where: {id: data.id}});
+            return res.status(200).json({ result });
+        } catch (error) {
+            return res.status(200).json({ message: '사용자 저장 중 오류 발생', error: error.message });
+        }
     }
-
-    return res.status(200).json({ user });
-
 })

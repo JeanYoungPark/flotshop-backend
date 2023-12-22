755f6921267e52b4637b109591eb3ce0730587b2
JeanYoungPark <jean.young.park.96@gmail.com>
2023-12-22 16:02:44 +0900

    스웨거 적용 작업 진행중

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { swaggerUi, specs } from '#core/swagger/swagger';
 
 export const app = express();
 const port = 3001;
@@ -15,6 +16,8 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs))
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);

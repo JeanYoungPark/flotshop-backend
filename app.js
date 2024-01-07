184202d4d0ab7d8698b2c6ef6a25b2c2c91fe303
giho <seonglf@rebeltech.info>
2024-01-07 16:40:46 +0900

    feat: 옵션 등록 기능 구현

--- app.js
+++ app.js
@@ -8,16 +8,18 @@ import { categoryRouter } from '#route/category';
 import { userRouter } from '#route/user';
 import { specs, swaggerUi } from './src/swagger/swagger.js';
 import { boardRouter } from '#route/board';
+import { optionRouter } from '#route/admin/option';
 
-const dirname = process.cwd();
 const api = express.Router();
 const admin = express.Router();
 
 app.use("/api", api);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use(express.static("uploads"));
 
 api.use(authRouter);
 api.use(categoryRouter);
+api.use(optionRouter)
 api.use(boardRouter)
 api.use(userRouter);
 

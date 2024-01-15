d180f1b9aa3c2341fe71ba2fa44f084da6140387
JeanYoungPark <jean.young.park.96@gmail.com>
2024-01-15 20:30:31 +0900

    폴더 재구성

--- src/routes/user/auth.js
+++ src/routes/user/auth.js
@@ -81,17 +81,3 @@ authRouter.post('/login', async(req, res) => {
     }
 
 })
-
-authRouter.post('/logout', async(req, res) => {
-    const data = req.body;
-    
-    const date = new Date();
-    date.setHours(date.getHours() + 9);
-
-    try {
-        const newSession = await Session.update({expired_at: date, updated_at: date}, {where: { id: data.id }});
-        return res.status(200).json({success: newSession});
-    } catch (error) {
-        return res.status(200).json({ message: '세션 변경 중 오류 발생', error: error.message });
-    }
-})
